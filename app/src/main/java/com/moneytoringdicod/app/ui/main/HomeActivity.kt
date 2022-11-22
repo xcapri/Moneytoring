249bc7ae0075356870413952b437d256d8571cb4
xcapri <asarhasyim@gmail.com>
2022-11-22 10:03:33 +0700

    Add structure

---
+++ app/src/main/java/com/moneytoringdicod/app/ui/main/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.moneytoringdicod.app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moneytoringdicod.app.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/moneytoringdicod/app/ui/main/PengeluaranActivity.kt
@@ -0,0 +1,12 @@
+package com.moneytoringdicod.app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moneytoringdicod.app.R
+
+class PengeluaranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengeluaran)
+    }
+}
